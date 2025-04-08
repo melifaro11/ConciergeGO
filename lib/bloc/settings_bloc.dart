a83c97b50ebd3fe40d64d13ab6980fa80e3d9919
Alexander <melifaro09@gmail.com>
2025-04-08 20:59:31 +0200

    Text animation

--- lib/bloc/settings_bloc.dart
+++ lib/bloc/settings_bloc.dart
@@ -5,8 +5,8 @@ import 'package:conciergego/bloc/states/settings_state.dart';
 import 'package:conciergego/models/settings_model.dart';
 import 'package:conciergego/services/firestore/firestore_service.dart';
 import 'package:conciergego/services/firestore/firestore_settings.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 
 class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
   final SettingsFirestoreService _fireStore = SettingsFirestoreService();
@@ -21,21 +21,29 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
   }
 
   void _onSettingsStreamUpdateEvent(
-      SettingsStreamUpdateEvent event, Emitter<SettingsState> emitter) async {
+    SettingsStreamUpdateEvent event,
+    Emitter<SettingsState> emitter,
+  ) async {
     emitter(SettingsLoadedState(settings: event.settings));
   }
 
   void _onThemeChangeEvent(
-      ThemeChangeEvent event, Emitter<SettingsState> emitter) {
+    ThemeChangeEvent event,
+    Emitter<SettingsState> emitter,
+  ) {
     if (state is SettingsLoadedState) {
-      _fireStore.updateSettings((state as SettingsLoadedState)
-          .settings
-          .copyWith(darkTheme: event.darkTheme));
+      _fireStore.updateSettings(
+        (state as SettingsLoadedState).settings.copyWith(
+          darkTheme: event.darkTheme,
+        ),
+      );
     }
   }
 
   void _onLoadUserSettingsEvent(
-      LoadUserSettingsEvent event, Emitter<SettingsState> emitter) async {
+    LoadUserSettingsEvent event,
+    Emitter<SettingsState> emitter,
+  ) async {
     try {
       _settingsSubscription?.cancel();
       _settingsSubscription = _fireStore.getSettingsStream().listen((settings) {
@@ -47,7 +55,9 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
   }
 
   void _onSettingsUpdateEvent(
-      SettingsUpdateEvent event, Emitter<SettingsState> emitter) async {
+    SettingsUpdateEvent event,
+    Emitter<SettingsState> emitter,
+  ) async {
     try {
       _fireStore.updateSettings(event.settings);
     } on FirestoreServiceException catch (e) {
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:conciergego/bloc/auth_bloc.dart';
 import 'package:conciergego/bloc/events/settings_event.dart';
 import 'package:conciergego/bloc/settings_bloc.dart';
@@ -22,14 +24,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  var isDarkTheme = true;
+
+  late AnimationController _controller;
+
   @override
   void initState() {
     super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    )..repeat();
   }
 
   @override
   Widget build(BuildContext context) {
-    var isDarkTheme = true;
+    //var isDarkTheme = true;
     final settingsState = context.watch<SettingsBloc>().state;
 
     if (settingsState is SettingsInitialState) {
@@ -46,7 +57,32 @@ class HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
     });
 
     if (settingsState is SettingsInitialState) {
-      return const Center(child: CircularProgressIndicator());
+      return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedBuilder(
+                animation: _controller,
+                builder: (context, child) {
+                  final opacity = sin(_controller.value * 2 * pi) * 0.35 + 0.65;
+                  return Opacity(
+                    opacity: opacity.clamp(0.2, 0.8),
+                    child: Text(
+                      "Connect to the server...",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  );
+                },
+              ),
+
+              const SizedBox(height: 15),
+              const Center(child: CircularProgressIndicator()),
+            ],
+          ),
+        ),
+      );
     } else if (settingsState is SettingsLoadedState &&
         settingsState.settings.openaiKey.isNotEmpty) {
       return Scaffold(
