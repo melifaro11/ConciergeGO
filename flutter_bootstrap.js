620116454d8c4f7d3ab9fd0efdffaf0444ba70f8
melifaro11 <melifaro11@users.noreply.github.com>
2025-04-14 09:11:30 +0000

    deploy: 6000d853830dac95ffda95d8a94b392ff1728543

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1064173312"
+    serviceWorkerVersion: "3245564697"
   }
 });
